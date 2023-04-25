949f9a223d3ee54f27a4ca0123f5bf19c3b62a04
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2023-04-25 21:08:05 +0000

    added _printf.c

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+
+/**
+ * _printf - writes formatted output to stdout
+ * @format: The format string
+ *
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+int i = 0;
+int count = 0;
+
+va_start(args, format);
+
+while (format && format[i])
+{
+if (format[i] == '%')
+{
+i++;
+
+switch (format[i])
+{
+case 'c':
+count += _putchar(va_arg(args, int));
+break;
+case 's':
+count += _putstring(va_arg(args, char *));
+break;
+case '%':
+count += _putchar('%');
+break;
+default:
+count += _putchar('%');
+if (format[i])
+count += _putchar(format[i]);
+break;
+}
+}
+else
+count += _putchar(format[i]);
+
+i++;
+}
+
+va_end(args);
+return (count);
+}
+
+
