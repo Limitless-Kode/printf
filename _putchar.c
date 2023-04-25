ea4bcc1122df73742dbe9cbcd2e40f588a426772
codyngpriest <vilhobanike@gmail.com>
2023-04-25 22:51:47 +0000

    updated _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -8,5 +8,18 @@
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+int bytes_written = 0;
+int total_bytes = 1;
+while (bytes_written < total_bytes)
+{
+int result = write(1, &c + bytes_written, total_bytes - bytes_written);
+if (result == -1)
+{
+/* Error handling code here */
+return (-1);
 }
+bytes_written += result;
+}
+return (1);
+}
+
