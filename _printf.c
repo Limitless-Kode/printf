398622e5b79470e6c218647871188ea9e1778960
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2023-04-25 21:20:20 +0000

    bug fix

--- _printf.c
+++ _printf.c
@@ -9,44 +9,44 @@
  */
 
 int _printf(const char *format, ...) {
-  int count = 0;
-  va_list args;
-  va_start(args, format);
+int count = 0;
+va_list args;
+va_start(args, format);
 
-  while (*format) {
-    if (*format == '%') {
-      format++;
-      switch (*format) {
-        case 'c': {
-          char c = va_arg(args, int);
-          write(1, &c, 1);
-          count++;
-          break;
-        }
-        case 's': {
-          char *s = va_arg(args, char *);
-          write(1, s, strlen(s));
-          count += strlen(s);
-          break;
-        }
-        case '%': {
-          write(1, "%", 1);
-          count++;
-          break;
-        }
-        default: {
-          // Unknown format specifier.
-          break;
-        }
-      }
-    } else {
-      write(1, format, 1);
-      count++;
-    }
-    format++;
-  }
+while (*format) {
+if (*format == '%') {
+format++;
+switch (*format) {
+case 'c': {
+char c = va_arg(args, int);
+write(1, &c, 1);
+count++;
+break;
+}
+case 's': {
+char *s = va_arg(args, char *);
+write(1, s, strlen(s));
+count += strlen(s);
+break;
+}
+case '%': {
+write(1, "%", 1);
+count++;
+break;
+}
+default: {
+// Unknown format specifier.
+break;
+}
+}
+} else {
+write(1, format, 1);
+count++;
+}
+format++;
+}
 
-  va_end(args);
-  return count;
+va_end(args);
+return count;
 }
 
