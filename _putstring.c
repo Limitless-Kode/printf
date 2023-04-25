385d7356b95167537c1b5020ea4eecf33d3d4ced
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2023-04-25 21:07:44 +0000

    added _putstring.c

---
+++ _putstring.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _putstring - writes a string to the standard output
+ * @str: The string to print
+ *
+ * Return: The number of characters written.
+ */
+int _putstring(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+
+return (i);
+}
