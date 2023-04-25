57ccbcec33d3b02fca4ff10d8c86bcf2034ee0bf
codyngpriest <vilhobanike@gmail.com>
2023-04-25 22:59:24 +0000

    updated _putint.c

--- _putint.c
+++ _putint.c
@@ -1,27 +1,31 @@
 #include "main.h"
-
 /**
   * _putint - puts an an int to stdout
   *
   * @n: the integer to print
   * Return: Always 0 (Success)
   */
-
 int _putint(int n)
 {
 unsigned int num;
-
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
 if (n < 0)
 {
 _putchar('-');
 num = -n;
 }
 else
+{
 num = n;
-
+}
 if (num / 10)
+{
 _putint(num / 10);
-
+}
 _putchar(num % 10 + '0');
 return (0);
 }
